4f36a7bb38daaf516df61c520abeb5f16624f2df
Prashant Rajput <105821189+Prashant7717@users.noreply.github.com>
2025-06-04 04:15:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
